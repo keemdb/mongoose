72bf8ec67f00e6512c8bd7516ac3707dc608fe5c
Dongbeom Kim <keemdb@gmail.com>
2022-03-29 18:48:01 +0900

    mongoose connect

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://keemdb2:test1234@boilerplate.v3hxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true, 
-	useCreateIndex: true, 
-	useFindAndModify: false
-})
+
+mongoose.connect('mongodb+srv://keemdb:test1234@boilerplate.v3hxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=> console.log('MongoDB Connected...'))
+  .catch((err) => console.log('err'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
